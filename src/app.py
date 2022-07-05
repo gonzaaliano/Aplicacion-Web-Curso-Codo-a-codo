2be2991fc9e8bd62e1083a0f74cbf802fffa5fad
Gonza Aliano <gonzaaliano@hotmail.com>
2022-07-04 21:43:36 -0300

    Deploy index.html from flask

--- src/app.py
+++ src/app.py
@@ -2,8 +2,28 @@ from flask import Flask
 from flask import render_template
 from flaskext.mysql import MySQL
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='empleados')
 mysql = MySQL()
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Galiano01*'
+app.config['MYSQL_DATABASE_DB'] = 'empleados'
+
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    
+    sql = "insert into empleados (nombre, correo, foto) values ('Juan', 'juan@email.com', 'fotodejuan.jpg');"
+    cursor.execute(sql)
+
+    conn.commit()
+
+    return render_template('empleados/index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
